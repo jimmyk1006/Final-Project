71a6159d8a3ca6b1a39f1c14223e9f7ca9451937
jimmyk1006 <Pr0m3d1@>
2019-12-07 12:18:44 -0500

    Created first Code for Character Creation

--- BossFight3.py
+++ BossFight3.py
@@ -1,17 +1,17 @@
 #Intro to my program of mini Boss fight 3
-print ("This is the second Mini Boss Fight Program, Please read the following:")
+print ("This is the thrid Mini Boss Fight Program, Please read the following:")
 print ("You will use your character that you created from the character creation program to defeat a boss.")
 import time
 time.sleep(2)
-print ("Welcome to Mind Flare's Chambers, Second Mini Boss Fight")
-print ("You will be fighting Mind Flare the Vision Seeker, as his name implies, he uses mind powers to try"
-       " and take over the world, killing all he sees.")
+print ("Welcome to Gilgrogs's Hollow, Thrid Mini Boss Fight")
+print ("You will be fighting Gilgrog the flesh reaper, as his name implies, he loves to rip all the flesh"
+       " off of his enemies and enjoys its as his meals.")
 time.sleep(2)
 print ("Please Read all Questions and Instructions Carefully, Hope you enjoy")
 
 #Here I import my variable from the Toon Creator
 from CharacterInfo import name, race_choice, class_choice, armor_choice, armor_color, specialabil
-from bossfightfunctions import westchamber, bossfight2
+from bossfightfunctions import leftentrance, bossfight3
 
 #hitpoints
 player_hitpoints = 100
@@ -20,36 +20,43 @@ time.sleep(2)
 
 
 #Start of the dungeon
-print (name, "flys into Mind Flares's Chambers.", name, "sees two chambers, one to going to the west and one going to the east, only one chamber will allow you into the chambers!")
+print (name, "rides his trusty stead into Gilgrog's Hollow.", name, "sees two gated entrances, one to the left and one to the right, only one gated entrance will allow you into the hollow!")
 time.sleep(2)
 print("Choose your decsion wisely!!!!")
 time.sleep(2)
-print ("What chamber do you want to fly into?")
-chamber_choice = input("Choose West or East:")
+print ("What gated entrance do you want to ride your trusty stead into?")
+entrance_choice = input("Choose Left or Right:")
 
-print (name, "has chosen", chamber_choice, "chamber...good luck")
+print (name, "has chosen", entrance_choice, "gated entrance...good luck")
 time.sleep(2)
 
-#East Chamber
-if chamber_choice == "East":
-       print(name, "flys into the east chamber and falls into a pit of exploding stars and dies!!")
+#Right Entrance
+if entrance_choice == "Right":
+       print(name, "rides his trusty stead into the right entrance and trys to open but its locked!")
+       time.sleep(2)
+       print(name, "trys to use his special ability of", specialabil, "to open the gate but when", specialabil, "hits the gate...it explodes")
+       time.sleep(2)
+       print("Gilgrog riged the gate with explosives and you exploded in the attempt of breaking the gate down...you die!")
+       time.sleep(2)
        print("GAME OVER!")
 else:
-       westchamber()
+       leftentrance()
 
 
-#Knob Fliers defeated now to boss!
-print ("Are you ready to fight Mind Flare the Vision Seeker?")
+#Gilgrogs minions defeated now to boss!
+print ("Are you ready to fight Gilgrog the flesh reaper?")
 readycheck = input("Answer Yes or No:")
 
-if armor_choice == 'Cloth':
-       print("Mind Flare laughs at", name,armor_choice, "armor", "and smacks all the", armor_choice, "armor off of", name)
+if race_choice == 'Orc':
+       print("Gilgrog laughs at", name, "because he is an Orc. Gilgrog has a natural immunity to orcs...")
        time.sleep(2)
-       print("Mind Flare kills you game over!")
+       print("Gilgrog spits green slime all over you...you dissolve and die")
+       time.sleep(2)
+       print("Gilgrog kills you game over!")
 
 elif readycheck == 'Yes':
-       bossfight2()
+       bossfight3()
 else:
-       print("To bad Mind Flare doesn't care, he takes control of your mind and flys you into mouth.... you die!")
+       print("To bad Gilgrog doesn't care, he spits green slime all over and it dissovles your brain.... you die!")
        time.sleep(2)
        print("GAME OVER!")
